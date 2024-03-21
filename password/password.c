15acbde26fb748e411d3707af0d48848eae7679e
thamac <45488745+thamac@users.noreply.github.com>
2024-03-21 19:51:16 +0100

    Add files via upload

---
+++ password/password.c
@@ -0,0 +1,59 @@
+// Check that a password has at least one lowercase letter, uppercase letter, number and symbol
+// Practice iterating through a string
+// Practice using the ctype library
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+bool valid(string password);
+
+int main(void)
+{
+    string password = get_string("Enter your password: ");
+    if (valid(password))
+    {
+        printf("Your password is valid!\n");
+    }
+    else
+    {
+        printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol\n");
+    }
+}
+
+// TODO: Complete the Boolean function below
+bool valid(string password)
+{
+    bool lowercase = false;
+    bool uppercase = false;
+    bool number = false;
+    bool symbol = false;
+
+    for (int i = 0, len = strlen(password); i < len; i++)
+    {
+        if (isupper(password[i]))
+        {
+            uppercase = true;
+        }
+        else if (islower(password[i]))
+        {
+            lowercase = true;
+        }
+        else if (isdigit(password[i]))
+        {
+            number = true;
+        }
+        else if (ispunct(password[i]))
+        {
+            symbol = true;
+        }
+    }
+
+    if (lowercase == true && uppercase == true && number == true && symbol == true)
+    {
+        return true;
+    }
+
+    return false;
+}
